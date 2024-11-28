4b9d9a458e688c572c613fc2560bb24971489b9b
Umut Yenidil <umut.yenidil9@gmail.com>
2024-11-28 23:16:36 +0300

    Initial commit

---
+++ src/test/java/com/umutyenidil/SpringChat/SpringChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umutyenidil.SpringChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
